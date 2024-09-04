6eafdb6b345bcfd8baa35a38eaab506adb085217
Mostafa14211 <hebeeshmostafa@gmail.com>
2024-09-04 15:27:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let container = document.getElementById('container');
 function setRole(roleId){
   let roleText = document.createTextNode(roleId);
   localStorage.setItem('role',roleText);
-  container.innerHTML = roleText;
+  container.appendChild(roleText);
   
 }
